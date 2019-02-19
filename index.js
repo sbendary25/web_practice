6f9f2dece8b136043889f1ade0ca404dca074c69
samerbendary <samer.a.bendary@vanderbilt.edu>
2019-02-18 22:37:10 -0600

    express server set up

--- index.js
+++
@@ -1,5 +0,0 @@
-const logSomething = options => ({ ...options, anotherOption: 'HellO!' });
-
-const options = logSomething({ one: 1, two: 2 });
-
-console.log(options);
---
+++ routes/index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const router = express.Router();
+
+/* Get home page */
+router.get('*', (req,res) => {
+  res.render('index');
+
+});
+
+export default router;
